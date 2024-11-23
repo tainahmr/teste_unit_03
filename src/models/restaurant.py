4c84faf7e5709da37fa71168aa6c2913fe9cfacb
tainahmr <tainahmr@gmail.com>
2024-11-23 15:45:51 -0300

    Levantamento de bug

--- src/models/restaurant.py
+++ src/models/restaurant.py
@@ -9,14 +9,20 @@ class Restaurant:
 
     def describe_restaurant(self):
         """Imprima uma descrição simples da instância do restaurante."""
+        #BUG: o result está escrito ""restaturante"
+        #BUG: o primeiro result não chama o restaurant_name
         result = f"Esse restaturante chama {self.cuisine_type} and serve {self.cuisine_type}."
         result += f"Esse restaturante está servindo {self.number_served} consumidores desde que está aberto."
         return result
 
     def open_restaurant(self):
         """Imprima uma mensagem indicando que o restaurante está aberto para negócios."""
+        #BUG: no if not o self.open não é false
+        #CORREÇÃO: self.open = True
         if not self.open:
             self.open = False
+            #BUG: O number_served não pode ser negativo, não é utilizado nessa função
+            #CORREÇÃO: o number_served deve ser 0
             self.number_served = -2
             return f"{self.restaurant_name} agora está aberto!"
         else:
@@ -33,6 +39,7 @@ class Restaurant:
 
     def set_number_served(self, total_customers):
         """Defina o número total de pessoas atendidas por este restaurante até o momento."""
+        #BUG: falta return do if
         if self.open:
             self.number_served = total_customers
         else:
@@ -40,6 +47,8 @@ class Restaurant:
 
     def increment_number_served(self, more_customers):
         """Aumenta número total de clientes atendidos por este restaurante."""
+        #BUG: não está incrementando o número
+        # BUG: falta return do if
         if self.open:
             self.number_served = more_customers
         else:
